9a501c63490056367346ee4c67c43016b34f455d
Omolade11 <omoladeekpeni@gmail.com>
2022-10-21 07:32:33 +0100

    update

--- cypress.config.js
+++ cypress.config.js
@@ -8,11 +8,17 @@ module.exports = defineConfig({
     },
   },
 
+  env: {
+    login_url: "https://demo.oryxhr.com",
+    email: "admin_demo@oryxhr.com",
+    password: "Oryx@108demo"
+  },
+
   component: {
     devServer: {
       framework: "angular",
       bundler: "webpack",
     },
     specPattern: "**/*.cy.ts",
-  },
+  }
 });
